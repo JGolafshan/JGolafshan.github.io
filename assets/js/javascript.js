3837bbe2d8b356f8bf56a912a88d66af4c818551
Brad Traversy <MD@BRADTRAVESY.COM>
2024-01-11 11:04:43 +1100

    Updated Website Design, Project/Skills need work

--- assets/js/javascript.js
+++
@@ -1,9 +0,0 @@
-var button = document.querySelector('.navbar-toggler');
-var span = document.querySelector('.navbar-toggler-icon');
-
-// Add a click event listener to the button
-button.addEventListener('click', function() {
-  // Toggle the classes on the span element
-  span.classList.toggle('bi-x-lg');
-  span.classList.toggle('navbar-toggler-icon');
-});
